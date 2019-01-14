d0a64c1bfbdba6bbbec9c0d664fd8fe2b9351260
sank3t <sanketsharma2196@gmail.com>
2019-01-14 14:52:28 +0530

    Changes made.

--- bm25.py
+++ bm25.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import xml.etree.ElementTree as ET
 import term_frequency as tf
-import inverse_doc_frequency as idf
+import okapi_modules as okapi
 from nltk.tokenize import regexp_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
@@ -44,16 +44,23 @@ b = 0.75
 k3 = 7.0
 qtf = 1
 
-# Calculating document length
-for doc_no, child in enumerate(root, start=1):
-    doc = regexp_tokenize(child[0].text, pattern)
-    doc = [d.lower() for d in doc]
+# Top documents
+query_top5_docs = {}
+query_top10_docs = {}
+query_top15_docs = {}
+query_top25_docs = {}
 
-    # Removing stopwords from document
-    doc = [w for w in doc if not w in stop_words]
-    doc_length['Doc ' + str(doc_no)] = len(doc)
+# top list of documents
+top5= []
+top10 = []
+top15 = []
+top25 = []
 
-avg_doc_length = round(sum(doc_length.values()) / N, 3)
+# Getting document length
+doc_length = okapi.document_length(root, pattern, stop_words)
+
+# Getting average document length
+avg_doc_length = okapi.average_document_length(doc_length, N)
 
 # Calculating term frequency
 for q_no, query in enumerate(queries['Query'], start=1):
@@ -95,36 +102,24 @@ for query in queries['Query']:
             if query_idf[q] > 0:
                 query_idf[q] = round(np.log2((N - query_idf[q] + 0.5)/ (query_idf[q] + 0.5)), 3)
 
-q_no = 0
-
 # Pseudo relevance feedback based selection Okapi-BM25
-for query in queries['Query']:
-    query = regexp_tokenize(query, pattern)
-    query = [q.lower() for q in query]
-    # Removing stopwords from query
-    query = [w for w in query if w not in stop_words]
-    query = set(query)
-
-    q_no += 1
-
+for q_no in range(1, 65):
     doc_score = {}
-
-    for tf in query_tf:
+    query = 'Query ' + str(q_no)
+    for i in range(N):
         score = 0
-        for i in range(N):
-
-            if len(query_tf[tf][i]) > 0:
-                for term in query_tf[tf][i]:
-                    K = k1 * ((1 - b) + (b * (doc_length['Doc '+str(i+1)] / avg_doc_length)))
-                    temp = query_idf[term] * ((k1+1) * query_tf[tf][i][term]) * (k3 + 1)
-                    score += round(temp / ((K + query_tf[tf][i][term]) *(k3+1)), 3)
-
-                doc_score['Doc ' + str(i+1)] = score
-            else:
-                doc_score['Doc ' + str(i+1)] = 0.0
+        if len(query_tf[query][i]) > 0:
+            for term in query_tf[query][i]:
+                K = k1 * ((1 - b) + (b * (doc_length['Doc ' + str(i + 1)] / avg_doc_length)))
+                temp = query_idf[term] * ((k1 + 1) * query_tf[query][i][term]) * (k3 + 1)
+                score += round(temp / ((K + query_tf[query][i][term]) * (k3 + 1)), 3)
+        doc_score['Doc ' + str(i + 1)] = score
 
     scores['Query ' + str(q_no)] = doc_score
 
-print(scores['Query 1'])
-print(scores['Query 2'])
-print(scores.keys())
+for score in scores:
+    query_top5_docs[score] = okapi.to_dict(sorted(scores[score].items(), key=lambda x:x[1], reverse=True)[:5])
+    query_top10_docs[score] = okapi.to_dict(sorted(scores[score].items(), key=lambda x:x[1], reverse=True)[:10])
+    query_top15_docs[score] = okapi.to_dict(sorted(scores[score].items(), key=lambda x: x[1], reverse=True)[:15])
+    query_top25_docs[score] = okapi.to_dict(sorted(scores[score].items(), key=lambda x: x[1], reverse=True)[:25])
+
--- okapi_modules.py
+++ okapi_modules.py
@@ -1,40 +1,26 @@
-import numpy as np
-
-k1 = 1.2
-b = 0.75
-k3 = 7
-
-def term_frequency(query, documents):
-    for q in query:
-        for child in documents:
-            temp = child[1].text.split()
-            if q in temp:
-                index = temp.index()
+from nltk.tokenize import regexp_tokenize
 
 
-def idf(N:int, n:int):
-    '''
-    Calculation of idf for term q-i in Query Q
+def document_length(root, pattern, stop_words):
+    doc_length = {}
+    for doc_no, child in enumerate(root, start=1):
+        doc = regexp_tokenize(child[0].text, pattern)
+        doc = [d.lower() for d in doc]
 
-     N: No. of documents in the collection
-     query_term: i-th query term in the query Q
-     n: No. of documents containing q-i
-
-     return: idf value
-    '''
-    return np.log10((N - n + 0.5) / (n + 0.5))
-
-def document_length(root):
-    '''
-    Calculating the document length
-    :return: document length
-    '''
-    doc_length = []
-    for child in root:
-        doc_length.append(len(child[1].text))
+        # Removing stopwords from document
+        doc = [w for w in doc if not w in stop_words]
+        doc_length['Doc ' + str(doc_no)] = len(doc)
 
     return doc_length
 
 
-def average_document_length():
-    doc_length =
+def average_document_length(doc_length, N):
+    return round(sum(doc_length.values()) / N, 3)
+
+def to_dict(sorted_list):
+    d = {}
+
+    for l in sorted_list:
+        d[l[0]] = l[1]
+
+    return d
