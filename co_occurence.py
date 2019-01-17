b4dcffa70ea5862f5a27dd855b3b1a0f6f8b0931
sank3t <sanketsharma2196@gmail.com>
2019-01-18 01:30:58 +0530

    Pool terms generated

--- co_occurence.py
+++ co_occurence.py
@@ -1,18 +1,22 @@
 import xml.etree.ElementTree as et
 import json
+from nltk.tokenize import regexp_tokenize
 
 
-def xml_to_dict(file_name: str):
+def xml_to_dict(file_name: str, stop_words, pattern):
     docs = {}
 
     tree = et.parse(file_name)
     root = tree.getroot()
 
     for index, child in enumerate(root, start=1):
-        description = child[0].text.replace('\n', '')
-        description = description.replace('\t', '')
-        description = description.lstrip().rstrip()
-        docs['Doc ' + str(index)] = description
+        doc = regexp_tokenize(child[0].text, pattern)
+        doc = [d.lower() for d in doc]
+
+        # Removing stopwords from document
+        doc = [w for w in doc if w not in stop_words]
+        doc = set(doc)
+        docs['Doc ' + str(index)] = ' '.join(doc)
 
     return docs
 
@@ -24,3 +28,15 @@ def json_to_dict(file_name: str):
         scores = json.load(fp)
 
     return scores
+
+
+def term_pool(scores_dict: dict, documents: dict):
+    terms = {}
+
+    for query in scores_dict:
+        terms[query] = {}
+
+        for doc in scores_dict[query]:
+            terms[query][doc] = documents[doc]
+
+    return terms
--- pool_terms.py
+++ pool_terms.py
@@ -1,13 +1,25 @@
 from glob import glob
 import co_occurence as co_occur
+from nltk.corpus import stopwords
 
 # JSON files
 files = sorted(glob('data/*.json'))
 
+# Stopwords
+stop_words = set(stopwords.words('english'))
+
+# Pattern keeping only digits and strings
+pattern = r'(\d+|\w+)'
+
 # Documents
-documents = co_occur.xml_to_dict('data/doc.xml')
+documents = co_occur.xml_to_dict('data/doc.xml', stop_words, pattern)
 
 top10_docs = co_occur.json_to_dict(files[0])
 top15_docs = co_occur.json_to_dict(files[1])
 top25_docs = co_occur.json_to_dict(files[2])
 top5_docs = co_occur.json_to_dict(files[3])
+
+term_pool_top5 = co_occur.term_pool(top5_docs, documents)
+term_pool_top10 = co_occur.term_pool(top10_docs, documents)
+term_pool_top15 = co_occur.term_pool(top15_docs, documents)
+term_pool_top25 = co_occur.term_pool(top25_docs, documents)
